bef15739136fbf0b255c35df3484ce89e36186ad
Mona Moxie <moxie4lyf@gmail.com>
2024-09-28 14:26:25 +0200

    refactor: update styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/main.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import axios from "axios";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import router from "./router";
+window.axios = axios;
+const app = createApp(App);
 
-import App from './App.vue'
-import router from './router'
+app.use(createPinia());
+app.use(router);
 
-const app = createApp(App)
+app.mount("#app");
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+// window.axios.defaults.headers.common["Authorization"] =
+// "Bearer " + store.getters["auth/getAccessToken"];
