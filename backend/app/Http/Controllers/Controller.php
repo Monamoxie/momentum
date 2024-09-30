6b0df56ce8f83fc8a46ff9bfc9327a044c829ba7
Mona Moxie <moxie4lyf@gmail.com>
2024-09-30 08:55:17 +0200

    feat: create base response methods

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Response;
+
 abstract class Controller
 {
-    //
+    protected function errorResponse($message = null, $errors = [], $code = 500)
+    {
+        return Response::json([
+            'message' => $message == null && is_string($errors) ? $errors : $message,
+            'errors' => $errors,
+            'data' => [],
+            'status' => $code
+        ], $code);
+    }
+
+    protected function successResponse(
+        $message,
+        $data = [],
+        $code = 200
+    ) {
+        return Response::json([
+            'message' => $message,
+            'data' => $data,
+            'errors' => [],
+            'status' => $code
+        ], $code);
+    }
 }
